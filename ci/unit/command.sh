62d124a7895c51061f806c79ec7c626029b48d6e
Rob Bavey <rob.bavey@elastic.co>
2019-01-22 15:09:45 -0500

    Fix build issues with docker
    Fixes #28

--- ci/unit/command.sh
+++ ci/unit/command.sh
@@ -4,7 +4,7 @@
 set -ex
 
 export PATH=$PATH:$PWD/vendor/jruby/bin
-gem install bundler
+gem install bundler -v '< 2'
 cd ../plugins/this
 cp /usr/share/logstash/logstash-core/versions-gem-copy.yml /usr/share/logstash/logstash-core-plugin-api/versions-gem-copy.yml
 ./gradlew vendor
