9b584d5df5b6b382e45e09624304b1e3f75cd5e3
Rob Bavey <rob.bavey@elastic.co>
2020-01-09 14:50:28 -0500

    Update Jackson dependencies for CI tests (#46)
    * Update Jackson dependencies for CI tests
    * Also increase memory as 256m was not sufficient
    * Update reference to proton j

--- lib/logstash-input-azure_event_hubs.rb
+++ lib/logstash-input-azure_event_hubs.rb
@@ -2,7 +2,7 @@
 
 require 'jar_dependencies'
 require_jar('com.google.code.gson', 'gson', '2.8.5')
-require_jar('org.apache.qpid', 'proton-j', '0.33.1')
+require_jar('org.apache.qpid', 'proton-j', '0.33.3')
 require_jar('org.apache.logging.log4j', 'log4j-slf4j-impl', '2.9.1')
 require_jar('com.microsoft.azure', 'azure-eventhubs', '2.2.0')
 require_jar('com.microsoft.azure', 'qpid-proton-j-extensions', '1.1.0')
