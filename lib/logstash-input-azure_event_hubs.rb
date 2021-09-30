21aaa53ba27d638d71a731e881ad3608adf5e987
Ry Biesemeyer <yaauie@users.noreply.github.com>
2021-09-30 08:46:38 -0700

    deps: update to latest-available same-major dependencies (#67)
    * deps: update to latest-available same-major dependencies
    
    * bump version and update changelog

--- lib/logstash-input-azure_event_hubs.rb
+++ lib/logstash-input-azure_event_hubs.rb
@@ -2,10 +2,10 @@
 
 require 'jar_dependencies'
 require_jar('com.google.code.gson', 'gson', '2.8.5')
-require_jar('org.apache.qpid', 'proton-j', '0.33.3')
+require_jar('org.apache.qpid', 'proton-j', '0.33.9')
 require_jar('org.apache.logging.log4j', 'log4j-slf4j-impl', '2.9.1')
-require_jar('com.microsoft.azure', 'azure-eventhubs', '2.2.0')
-require_jar('com.microsoft.azure', 'qpid-proton-j-extensions', '1.1.0')
-require_jar('com.microsoft.azure', 'azure-eventhubs-eph', '2.4.0')
-require_jar('com.microsoft.azure', 'azure-storage', '8.0.0')
+require_jar('com.microsoft.azure', 'azure-eventhubs', '2.3.2')
+require_jar('com.microsoft.azure', 'qpid-proton-j-extensions', '1.2.4')
+require_jar('com.microsoft.azure', 'azure-eventhubs-eph', '2.5.2')
+require_jar('com.microsoft.azure', 'azure-storage', '8.6.6')
 require_jar('org.apache.logging.log4j', 'log4j-api', '2.9.1')
